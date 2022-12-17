80f06247155f1844bae82d02be5096ddce58c748
Mihails 'Delinx' Mozajevs <mihails@teamilluminate.dev>
2022-12-17 18:19:11 +0000

    fixed highlihgts of gitgutter

--- after/plugin/indent-line.lua
+++ after/plugin/indent-line.lua
@@ -1,4 +1,5 @@
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#333232 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#555555 gui=nocombine]]
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
@@ -7,5 +8,6 @@ require('indent_blankline').setup {
     show_trailing_blankline_indent = false,
     char_highlight_list = {
         "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
     },
 }
--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,33 +1,36 @@
-function ColorMyPencils(color) 
-	color = color or "gruvbox"
-	vim.cmd.colorscheme(color)
+function ColorMyPencils(color)
+    color = color or "gruvbox"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
-	vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#1c1c1c", bg = "black"})
-	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-	vim.api.nvim_set_hl(0, "TermCursor", {bg="black", fg="black"})
-    vim.api.nvim_set_hl(0, "CursorLine", {fg="none", bg="none"})
-    vim.api.nvim_set_hl(0, "CursorLineNR", {fg="#D79717", bg="none"})
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+    vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#1c1c1c", bg = "black" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TermCursor", { bg = "black", fg = "black" })
+    vim.api.nvim_set_hl(0, "CursorLine", { fg = "none", bg = "none" })
+    vim.api.nvim_set_hl(0, "CursorLineNR", { fg = "#D79717", bg = "none" })
+
+    vim.api.nvim_set_hl(0, "GitSignsAdd", { bg = "none", fg = "#98be65" })
+    vim.api.nvim_set_hl(0, "GitSignsChange", { bg = "none", fg = "#51afef" })
+    vim.api.nvim_set_hl(0, "GitSignsDelete", { bg = "none" ,fg = "#ec5f67" })
 end
 
 require("gruvbox").setup({
-			    undercurl = true,
-			    underline = true,
-			    bold = true,
-			    italic = false,
-			    strikethrough = true,
-			    invert_selection = false,
-			    invert_signs = false,
-			    invert_tabline = false,
-			    invert_intend_guides = false,
-			    inverse = true, -- invert background for search, diffs, statuslines and errors
-			    contrast = "hard", -- can be "hard", "soft" or empty string
-			    overrides = {
-                    SignColumn = {bg = "#ff9900"},
-			    },
-			    })
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = false,
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "hard", -- can be "hard", "soft" or empty string
+    overrides = {
+        SignColumn = { bg = "#ff9900" },
+    },
+})
 
 ColorMyPencils()
-
